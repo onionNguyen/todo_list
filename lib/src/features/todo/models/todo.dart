9f55e8c081e4cde3229d27b7133cfe91c5340653
Danh Tran <danhtran0312@gmail.com>
2023-05-29 17:43:31 -0500

    Add Date and Time picker

--- lib/src/features/todo/models/todo.dart
+++ lib/src/features/todo/models/todo.dart
@@ -4,38 +4,52 @@ import 'dart:convert';
 class Todo {
   Todo({
     required this.title,
+    required this.id,
     required this.description,
     this.isCompleted = false,
+    this.date,
   });
   final String title;
+  final String id;
   final String description;
   final bool isCompleted;
+  final DateTime? date;
 
   Todo copyWith({
     String? title,
+    String? id,
     String? description,
     bool? isCompleted,
+    DateTime? date,
   }) {
     return Todo(
       title: title ?? this.title,
+      id: id ?? this.id,
       description: description ?? this.description,
       isCompleted: isCompleted ?? this.isCompleted,
+      date: date ?? this.date,
     );
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'title': title,
+      'id': id,
       'description': description,
       'isCompleted': isCompleted,
+      'date': date?.millisecondsSinceEpoch,
     };
   }
 
   factory Todo.fromMap(Map<String, dynamic> map) {
     return Todo(
       title: map['title'] as String,
+      id: map['id'] as String,
       description: map['description'] as String,
       isCompleted: map['isCompleted'] as bool,
+      date: map['date'] != null
+          ? DateTime.fromMillisecondsSinceEpoch(map['date'] as int)
+          : null,
     );
   }
 
@@ -45,19 +59,27 @@ class Todo {
       Todo.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() =>
-      'Todo(title: $title, description: $description, isCompleted: $isCompleted)';
+  String toString() {
+    return 'Todo(title: $title, id: $id, description: $description, isCompleted: $isCompleted, date: $date)';
+  }
 
   @override
   bool operator ==(covariant Todo other) {
     if (identical(this, other)) return true;
 
     return other.title == title &&
+        other.id == id &&
         other.description == description &&
-        other.isCompleted == isCompleted;
+        other.isCompleted == isCompleted &&
+        other.date == date;
   }
 
   @override
-  int get hashCode =>
-      title.hashCode ^ description.hashCode ^ isCompleted.hashCode;
+  int get hashCode {
+    return title.hashCode ^
+        id.hashCode ^
+        description.hashCode ^
+        isCompleted.hashCode ^
+        date.hashCode;
+  }
 }
